dd056587a2ed0226eb8dfcf06a097553890b4f1f
U5 <kyutech.joko@gmail.com>
2025-10-15 19:00:13 +0900

    add:skill-data

--- src/components/Skills/index.tsx
+++ src/components/Skills/index.tsx
@@ -1,12 +1,18 @@
 import "./styles.css";
 import Title from "../ui/Title";
+import { skills } from "../../data/skills";
 
 const Skills = () => {
   return (
     <section id="skills" className="skills">
       <Title text="Skills" />
-      <ul>
-        <li>JavaScript</li>
+      <ul className="skill-items">
+        {skills.map((skill) => (
+          <li key={skill.name} className="skill-item">
+            <img src={skill.icon} alt={skill.name} className="skill-icon" />
+            <span>{skill.name}</span>
+          </li>
+        ))}
       </ul>
     </section>
   );
