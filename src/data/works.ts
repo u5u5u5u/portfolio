a486929e63734bc98984c442bc20f582421a49cf
U5 <kyutech.joko@gmail.com>
2025-10-18 03:00:06 +0900

    add:img-url-dev

--- src/data/works.ts
+++ src/data/works.ts
@@ -11,11 +11,15 @@ export const works: WorkType[] = [
     title: "e-ZUKA journey",
     description: "飯塚市の観光地を巡り、ARコンテンツで楽しむ観光案内アプリ",
     tags: ["Unity", "PlateauSDK", "VPS", "ARCore", "ARKit"],
+    imageUrl:
+      "https://pub-fcd0f7c6926e49c8b6b6e33b994dd2f0.r2.dev/e-zuka-journey/e-zuka-journey.png",
   },
   {
     title: "Kyutech Guide",
     description: "九州工業大学飯塚キャンパスを紹介するARを用いた大学案内アプリ",
     tags: ["Unity", "Geospatial API", "ARCore", "ARKit"],
+    imageUrl:
+      "https://pub-fcd0f7c6926e49c8b6b6e33b994dd2f0.r2.dev/kyutech-guide/kyutech-guide.png",
   },
   {
     title: "地球っ子ネットワーク HP",
@@ -29,5 +33,7 @@ export const works: WorkType[] = [
     title: "まちゃろぐ",
     description: "抹茶スイーツを共有するSNS",
     tags: ["Next.js", "TypeScript", "Supabase"],
+    imageUrl:
+      "https://pub-fcd0f7c6926e49c8b6b6e33b994dd2f0.r2.dev/matcha-log/matcha-log.png",
   },
 ];
