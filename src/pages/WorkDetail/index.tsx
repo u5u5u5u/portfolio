ff86217c896e1433be7cf4c99ae5b7f29278b1a6
U5 <kyutech.joko@gmail.com>
2025-11-12 02:42:49 +0900

    fix:見た目修正

--- src/pages/WorkDetail/index.tsx
+++ src/pages/WorkDetail/index.tsx
@@ -16,6 +16,7 @@ const WorkDetail = () => {
         }
         const data = await response.json();
         setWork(data);
+        console.log("Fetched work detail:", data);
       } catch (error) {
         console.error("Error fetching work detail:", error);
       }
@@ -39,30 +40,30 @@ const WorkDetail = () => {
     {
       label: "受賞",
       value: work?.awards?.map((award) => <p key={award}>{award}</p>),
-      condition: work?.awards,
+      condition: (work?.awards?.length ?? 0) > 0,
     },
     { label: "背景", value: work?.background, condition: work?.background },
     { label: "目的", value: work?.purpose, condition: work?.purpose },
     {
       label: "機能",
       value: work?.function?.map((func) => <p key={func}>{func}</p>),
-      condition: work?.function,
+      condition: (work?.function?.length ?? 0) > 0,
     },
     { label: "制作人数", value: `${work?.number}人`, condition: work?.number },
     {
       label: "担当",
       value: work?.role?.map((role) => <p key={role}>{role}</p>),
-      condition: work?.role,
+      condition: (work?.role?.length ?? 0) > 0,
     },
     {
       label: "発表",
       value: work?.presentation?.map((presen) => <p key={presen}>{presen}</p>),
-      condition: work?.presentation,
+      condition: (work?.presentation?.length ?? 0) > 0,
     },
     { label: "制作期間", value: work?.duration, condition: work?.duration },
     {
       label: "Webサイト",
-      value: <Link to={work?.webUrl || ""}>{work?.outname}</Link>,
+      value: <Link to={work?.webUrl || ""}>{work?.webUrl}</Link>,
       condition: work?.webUrl,
     },
     {
@@ -92,22 +93,25 @@ const WorkDetail = () => {
           <img src={work.thumbnail} alt={work.title} />
           <table>
             <tbody>
-              {tableRows.map(
-                (row, index) =>
-                  row.condition && (
-                    <tr key={index}>
-                      <th>{row.label}</th>
-                      <td
-                        className={
-                          row.label === "使用技術" || row.label === "機能"
-                            ? "techs"
-                            : ""
-                        }
-                      >
-                        {row.value}
-                      </td>
-                    </tr>
-                  )
+              {tableRows.map((row, index) =>
+                row.condition ? (
+                  <tr key={index}>
+                    <th>{row.label}</th>
+                    <td
+                      className={
+                        row.label === "使用技術" || row.label === "担当"
+                          ? "row"
+                          : row.label === "受賞" ||
+                            row.label === "機能" ||
+                            row.label === "発表"
+                          ? "column"
+                          : ""
+                      }
+                    >
+                      {row.value}
+                    </td>
+                  </tr>
+                ) : null
               )}
             </tbody>
           </table>
